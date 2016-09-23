fbaf0c9679789cfb196e89894b1c62579a1b8255
Robert Lee <lee.robert.138@gmail.com>
2016-09-23 15:32:51 -0400

    modified setup so it'll have version number and install dependencies

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
-from setuptools import setup
-setup(name='mml2sympy', packages=['mml2sympy'])
+from setuptools import setup, find_packages
+setup(
+	name='mml2sympy', 
+	version='0.1.0',
+	packages=find_packages(exclude=['tests']),
+	install_requires=['sympy', 'lxml'],
+)
